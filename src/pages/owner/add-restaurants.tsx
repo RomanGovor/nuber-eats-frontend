44a0bc2178cee3a49449b451254c1eea550eed1e
Govor <rgovor@ibagroup.eu>
2022-03-23 19:04:42 +0300

    fix: createAccount

--- src/pages/owner/add-restaurants.tsx
+++ src/pages/owner/add-restaurants.tsx
@@ -1,11 +1,11 @@
-import { gql, useApolloClient, useMutation } from "@apollo/client";
-import React, { useState } from "react";
-import { Helmet } from "react-helmet-async";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-import { Button } from "../../components/button";
-import { FormError } from "../../components/form-error";
-import { MY_RESTAURANTS_QUERY } from "./my-restaurants";
+import {gql, useApolloClient, useMutation} from "@apollo/client";
+import React, {useState} from "react";
+import {Helmet} from "react-helmet-async";
+import {useForm} from "react-hook-form";
+import {useNavigate} from "react-router-dom";
+import {Button} from "../../components/button";
+import {FormError} from "../../components/form-error";
+import {MY_RESTAURANTS_QUERY} from "./my-restaurants";
 import {
     createRestaurant,
     createRestaurantVariables,
@@ -25,23 +25,25 @@ interface IFormProps {
     name: string;
     address: string;
     categoryName: string;
-    file: FileList;
+    coverImg: string;
+    // file: FileList;
 }
 
 export const AddRestaurant = () => {
     const client = useApolloClient();
     const navigate = useNavigate();
-    const [imageUrl, setImageUrl] = useState("");
+    // const [imageUrl, setImageUrl] = useState("");
 
     const onCompleted = (data: createRestaurant) => {
         const {
-            createRestaurant: { ok, restaurantId },
+            createRestaurant: {ok, restaurantId},
         } = data;
+
         if (ok) {
-            const { name, categoryName, address } = getValues();
+            const {name, categoryName, address, coverImg} = getValues();
             setUploading(false);
 
-            const queryResult = client.readQuery({ query: MY_RESTAURANTS_QUERY });
+            const queryResult = client.readQuery({query: MY_RESTAURANTS_QUERY});
             client.writeQuery({
                 query: MY_RESTAURANTS_QUERY,
                 data: {
@@ -54,7 +56,7 @@ export const AddRestaurant = () => {
                                     name: categoryName,
                                     __typename: "Category",
                                 },
-                                coverImg: imageUrl,
+                                coverImg,
                                 id: restaurantId,
                                 isPromoted: false,
                                 name,
@@ -69,14 +71,12 @@ export const AddRestaurant = () => {
         }
     };
 
-    const [createRestaurantMutation, { data }] = useMutation<
-        createRestaurant,
-        createRestaurantVariables
-        >(CREATE_RESTAURANT_MUTATION, {
+    const [createRestaurantMutation, {data}] = useMutation<createRestaurant,
+        createRestaurantVariables>(CREATE_RESTAURANT_MUTATION, {
         onCompleted,
     });
 
-    const { register, getValues, formState, handleSubmit } = useForm<IFormProps>({
+    const {register, getValues, formState, handleSubmit} = useForm<IFormProps>({
         mode: "onChange",
     });
 
@@ -84,33 +84,41 @@ export const AddRestaurant = () => {
 
     const onSubmit = async () => {
         try {
-            setUploading(true);
+            const {name, categoryName, address, coverImg} = getValues();
 
-            const { file, name, categoryName, address } = getValues();
-            const actualFile = file[0];
-            const formBody = new FormData();
+            if (coverImg.endsWith('.png') || coverImg.endsWith('.jpeg') || coverImg.endsWith('.jpg')) {
+                setUploading(true);
 
-            formBody.append("file", actualFile);
-
-            const { url: coverImg } = await (
-                await fetch("http://localhost:4000/uploads/", {
-                    method: "POST",
-                    body: formBody,
-                })
-            ).json();
-            setImageUrl(coverImg);
-
-            createRestaurantMutation({
-                variables: {
-                    input: {
-                        name,
-                        categoryName,
-                        address,
-                        coverImg,
+                createRestaurantMutation({
+                    variables: {
+                        input: {
+                            name,
+                            categoryName,
+                            address,
+                            coverImg,
+                        },
                     },
-                },
-            });
-        } catch (e) {}
+                });
+            } else {
+                alert('Cover img must be .png, .jpeg or .jpg')
+            }
+
+            // const { file, name, categoryName, address } = getValues();
+
+            // const actualFile = file[0];
+            // const formBody = new FormData();
+
+            // formBody.append("file", actualFile);
+
+            // const { url: coverImg } = await (
+            //     await fetch("http://localhost:4000/uploads/", {
+            //         method: "POST",
+            //         body: formBody,
+            //     })
+            // ).json();
+            // setImageUrl(coverImg);
+        } catch (e) {
+        }
     };
 
     return (
@@ -128,21 +136,28 @@ export const AddRestaurant = () => {
                     type="text"
                     name="name"
                     placeholder="Name"
-                    ref={register({ required: "Name is required." })}
+                    ref={register({required: "Name is required."})}
                 />
                 <input
                     className="input"
                     type="text"
                     name="address"
                     placeholder="Address"
-                    ref={register({ required: "Address is required." })}
+                    ref={register({required: "Address is required."})}
                 />
                 <input
                     className="input"
                     type="text"
                     name="categoryName"
                     placeholder="Category Name"
-                    ref={register({ required: "Category Name is required." })}
+                    ref={register({required: "Category Name is required."})}
+                />
+                <input
+                    className="input"
+                    type="text"
+                    name="coverImg"
+                    placeholder="Link to image"
+                    ref={register({required: "Link to image is required."})}
                 />
                 {/*<div>*/}
                 {/*    <input*/}
@@ -158,7 +173,7 @@ export const AddRestaurant = () => {
                     actionText="Create Restaurant"
                 />
                 {data?.createRestaurant?.error && (
-                    <FormError errorMessage={data.createRestaurant.error} />
+                    <FormError errorMessage={data.createRestaurant.error}/>
                 )}
             </form>
         </div>
